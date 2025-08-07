fb74c6cb677e342a0e74eba3fbe1513c3bb7aed0
Aman-Ingale <ingaleaman905@gmail.com>
2025-08-07 22:56:36 +0530

    fixed crud/review.py

--- Backend/crud/review.py
+++ Backend/crud/review.py
@@ -1,8 +1,9 @@
-from database import db
+from database import get_db
 from models.review import Review
 
 def create_review(review: Review):
     try:
+        db = get_db()
         review_dict = review.dict()
         result = db.reviews.insert_one(review_dict)
         return True
