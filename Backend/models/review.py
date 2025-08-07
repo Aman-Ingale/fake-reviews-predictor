2b7e5b400dcbbb93c89ab7a16b027441a5a43155
Aman-Ingale <ingaleaman905@gmail.com>
2025-08-07 23:31:54 +0530

    removed database

--- Backend/models/review.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from datetime import datetime
-
-class Review(BaseModel):
-    text: str = Field(..., description="Text submitted by the user")
-    prediction: int = Field(..., ge=0, le=1, description="ML prediction: 1 = Negative/Fake, 0 = Positive/Genuine")
-    timestamp: datetime = Field(default_factory=datetime.utcnow, description="Time of prediction creation")
--- Backend/routes/review.py
+++
@@ -1,9 +0,0 @@
-# from fastapi import APIRouter
-# from ..models.review import Review
-
-
-# router = APIRouter()
-
-# @router.post("/predict")
-# def add_user(user: User):
-#     return {"id": }
